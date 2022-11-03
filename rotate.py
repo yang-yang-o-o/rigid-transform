b374442940d7a59017743a7e3a52f82d77b557f0
yang-yang-o-o <43899268+yang-yang-o-o@users.noreply.github.com>
2022-11-03 10:27:22 +0800

    1

--- rotate.py
+++ rotate.py
@@ -1,5 +1,5 @@
 import numpy as np
-import math
+from math import cos,sin
 
 # 绕x轴旋转
 
@@ -8,4 +8,14 @@ import math
 # 绕z轴旋转
 
 # 绕任意轴旋转
-
+def rotation_k_θ(k,θ):
+    '''
+    input:
+        k   :   [x,y,z], symmetry_axis
+        θ   :   rad, rotation angle
+    output:
+        R   :   rotation matrix R(k,θ)
+    '''
+    return np.array([[k[0]*k[0]*(1-cos(θ))+cos(θ)     , k[1]*k[0]*(1-cos(θ))-k[2]*sin(θ), k[2]*k[0]*(1-cos(θ))+k[1]*sin(θ)],
+                        [k[0]*k[1]*(1-cos(θ))+k[2]*sin(θ), k[1]*k[1]*(1-cos(θ))+cos(θ)     , k[2]*k[1]*(1-cos(θ))-k[0]*sin(θ)],
+                        [k[0]*k[2]*(1-cos(θ))-k[1]*sin(θ), k[1]*k[2]*(1-cos(θ))+k[0]*sin(θ), k[2]*k[2]*(1-cos(θ))+cos(θ)]])
